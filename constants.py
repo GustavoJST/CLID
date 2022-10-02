7e702084bd77168f5e0db0f42cfada27f249c545
GustavoJST <gutojusto22@gmail.com>
2022-10-02 03:17:02 -0300

    Added NO_SIZE_TYPE constant.

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
-# Since all downloads/uploads in this program are set to resumable=True, these actions
-# are performed in chunks. Set the desired chunk size (in bytes), in the
-# variable below.  
-# NOTE: There are some restrictions with chunk sizes. For files larger than 256KB,
-# use CHUNK_SIZE values that are multiples of 256KB (1024 * 256 * some_value).
-# For files smaller than 256KB, there are no restrictions.
-#  
+""" Since all downloads/uploads in this program are set to resumable=True, these actions
+are performed in chunks. Set the desired chunk size (in bytes), in the
+variable below.  
+NOTE: There are some restrictions with chunk sizes. For files larger than 256KB,
+use CHUNK_SIZE values that are multiples of 256KB (1024 * 256 * some_value).
+For files smaller than 256KB, there are no restrictions. """
+  
 # For the best efficiency, try to keep the chunk size as close to the default
 # value as possible.
 CHUNK_SIZE = 104857600  # Equals to 100MB. Default Google Drive chunk size value. 
@@ -17,16 +17,44 @@ GOOGLE_WORKSPACE_MIMETYPES = {"application/vnd.google-apps.script": "Script",
                             "application/vnd.google-apps.drawing" : "Drawing",
                             "application/vnd.google-apps.folder" : "Folder",
                             "application/vnd.google-apps.spreadsheet": "Spreadsheet",
-                            "application/vnd.google-apps.form": "Form",
-                            "application/vnd.google-apps.map": "Map",
-                            "application/vnd.google-apps.site": "Site",
+                            "application/vnd.google-apps.form": "*Form",
+                            "application/vnd.google-apps.map": "*Map",
+                            "application/vnd.google-apps.site": "*Site",
                             "application/vnd.google-apps.photo": "Photo"}
                             
-DRIVE_EXPORT_FORMATS = {"application/vnd.google-apps.script",
-                        "application/vnd.google-apps.presentation",
-                        "application/vnd.google-apps.jam",
-                        "application/vnd.google-apps.document",
-                        "application/vnd.google-apps.drawing",
-                        "application/vnd.google-apps.folder",
-                        "application/vnd.google-apps.spreadsheet"}
+DRIVE_EXPORT_FORMATS = {"application/vnd.google-apps.script": 
+                            [{"format": "JSON", "mimetype": "application/vnd.google-apps.script+json"}],
+                        
+                        "application/vnd.google-apps.presentation": 
+                            [{"format": "MS PowerPoint", "mimetype": "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+                            {"format": "PDF", "mimetype": "application/pdf"}],
+                        
+                        "application/vnd.google-apps.jam": 
+                            [{"format": "PDF", "mimetype": "application/pdf"}],
 
+                        "application/vnd.google-apps.document": 
+                            [{"format": "PDF", "mimetype": "application/pdf"}, 
+                            {"format": "HTML", "mimetype": "text/html"},
+                            {"format": "Plain Text", "mimetype": "text/plain"},
+                            {"format": "MS Word document", "mimetype": "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}],
+
+                        "application/vnd.google-apps.drawing": 
+                            [{"format": "JPEG", "mimetype": "image/jpeg"},
+                            {"format": "PNG", "mimetype": "image/png"},
+                            {"format": "SVG", "mimetype": "image/svg+xml"},
+                            {"format": "PDF", "mimetype": "application/pdf"}],
+
+                        #"application/vnd.google-apps.folder":
+                        #    [{"format": "ZIP", "mimetype": "application/zip"}],
+
+                        "application/vnd.google-apps.spreadsheet":
+                            [{"format": "MS Excel", "mimetype": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+                            {"format": "CSV", "mimetype": "text/csv"},
+                            {"format": "PDF", "mimetype": "application/pdf"}]}
+
+# Google Drive doesn't return the size for these types of files, or returns a
+# fixed value of 1KB.
+NO_SIZE_TYPES = ["application/vnd.google-apps.folder", 
+                "application/vnd.google-apps.map",
+                "application/vnd.google-apps.site",
+                "application/vnd.google-apps.form"]
